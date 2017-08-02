96585f0116b19db09b9d2642049ea848db897b7b
Paris Pantigoso <proinnovaperu@outlook.com>
2017-08-02 17:46:14 -0500

    Add project files.

---
+++ SearchFighter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchFighter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] query = { "The University of Hong Kong" };
+
+            foreach (string item in query)
+            {
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("Titulo: {0}", item);
+            }
+            Console.ReadKey();
+
+        }
+
+        //public static IList<Result> Search(string query)
+        //{
+        //    Console.WriteLine("Executing custom search for query: {0} ...", query);
+
+        //    CseResource.ListRequest listRequest = Service.Cse.List(query);
+        //    listRequest.Cx = cx;
+
+        //    Search search = listRequest.Execute();
+        //    return search.Items;
+        //}
+    }
+}
